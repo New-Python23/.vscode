ecdf4dbfb91ae5aa7a45a7720fbf84756a9c3559
New-Python23 <pythonbasictrue@gmail.com>
2022-11-12 22:40:52 -0800

    First Commit

--- python/basic_calculator.py
+++ python/basic_calculator.py
@@ -1,5 +1,15 @@
 while True:
     num1 = input("Please enter a number: ")
     num2 = input("Please enter another number: ")
+    num1 = int(num1)
+    num2 = int(num2)
     arithmetic = input("Please enter an arithmetic: ")
-    calculate = (int(num1) arithmetic int(num2))
+    if arithmetic == '+':
+        result = num1 + num2
+    elif arithmetic == '-':
+        result = num1 - num2
+    elif arithmetic == '*':
+        result = num1 * num2
+    elif arithmetic == '/':
+        result = num1 / num2
+    print(result)
