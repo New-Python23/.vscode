12da393fec4838f6ed7770358c1b622efc5199e5
New-Python23 <pythonbasictrue@gmail.com>
2022-11-12 22:36:56 -0800

    first commit

---
+++ python/basic_calculator.py
@@ -0,0 +1,5 @@
+while True:
+    num1 = input("Please enter a number: ")
+    num2 = input("Please enter another number: ")
+    arithmetic = input("Please enter an arithmetic: ")
+    calculate = (int(num1) arithmetic int(num2))
